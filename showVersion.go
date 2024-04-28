1b00662c0f9ab60e49045ed838a3cc77f71fc73c
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2024-04-28 19:55:49 +1200

    cicd: goreleaser pipeline (closes #5)

--- showVersion.go
+++ showVersion.go
@@ -2,13 +2,28 @@ package main
 
 import "fmt"
 
-// version of the program
-const version = "v0.1.0" //FUTURE: this should be set by the build system.
+// version information: set by the build process
+var (
+	commit    string
+	date      string
+	version   string
+	goVersion string
+	builtBy   string
+)
 
 // showVersion is a command that prints the version.
 type showVersion struct{}
 
 // run prints the version.
 func (showVersion) run(*opts) {
-	fmt.Printf("test-report %s\n", version)
+	if version == "" {
+		fmt.Println("test-report <unknown version>")
+		fmt.Println("(built from source)")
+		return
+	}
+	fmt.Printf("test-report v%s\n", version)
+	fmt.Println("commit:", commit)
+	fmt.Println("date:", date)
+	fmt.Println("built by:", builtBy)
+	fmt.Println("go version:", goVersion)
 }
--- showVersion_test.go
+++ showVersion_test.go
@@ -6,15 +6,62 @@ import (
 	"github.com/blugnu/test"
 )
 
+func using[T any](v *T, tv T) func() {
+	og := *v
+	*v = tv
+	return func() { *v = og }
+}
+
 func TestShowVersion(t *testing.T) {
 	// ARRANGE
-	cmd := &showVersion{}
+	testcases := []struct {
+		scenario string
+		exec     func(t *testing.T)
+	}{
+		{scenario: "version not set",
+			exec: func(t *testing.T) {
+				// ARRANGE
+				cmd := &showVersion{}
+				defer using(&version, "")()
 
-	// ACT
-	stdout, _ := test.CaptureOutput(t, func() {
-		cmd.run(nil)
-	})
+				// ACT
+				stdout, _ := test.CaptureOutput(t, func() {
+					cmd.run(nil)
+				})
 
-	// ASSERT
-	stdout.Equals([]string{"test-report v0.1.0"})
+				// ASSERT
+				stdout.Equals([]string{"test-report <unknown version>", "(built from source)"})
+			},
+		},
+		{scenario: "version set",
+			exec: func(t *testing.T) {
+				// ARRANGE
+				cmd := &showVersion{}
+				defer using(&version, "1.0.0")()
+				defer using(&commit, "commit")()
+				defer using(&date, "date")()
+				defer using(&builtBy, "builtBy")()
+				defer using(&goVersion, "goVersion")()
+
+				// ACT
+				stdout, _ := test.CaptureOutput(t, func() {
+					cmd.run(nil)
+				})
+
+				// ASSERT
+				stdout.Equals([]string{
+					"test-report v1.0.0",
+					"commit: commit",
+					"date: date",
+					"built by: builtBy",
+					"go version: goVersion",
+				})
+			},
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.scenario, func(t *testing.T) {
+			tc.exec(t)
+		})
+	}
 }
